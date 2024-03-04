43e19dff47b14f3016cb718e11a578ab10e03136
247769 Pruszyński Michał <247769@edu.p.lodz.pl>
2024-03-04 15:00:18 +0100

    Working program commit

---
+++ TPW/Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string a;
+        Console.WriteLine("type an all number string to convert it to int or ingame to get true'd");
+        a = Console.ReadLine();
+        Console.WriteLine(a);
+        Console.WriteLine(Equals(a, "ingame"));
+        int.TryParse(a, out int b);
+        if (b == 0)
+        {
+            Console.WriteLine("That is not an all number string");
+        }
+        else
+        {
+            Console.WriteLine(b);
+        }
+    }
+}
